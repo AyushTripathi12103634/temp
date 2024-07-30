bd5a475a17b894fa31db174de3af05f19231a1fe
Mohak1301 <mohaktiwari2004@gmail.com>
2024-07-30 19:40:46 +0530

    about

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 import Store from './Redux/Store';
 import NavBar from './Components/Navbar';
-
+import About from './Pages/About';
 function App() {
   return (
     <>
@@ -11,6 +11,9 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<NavBar />} />
+            <Route path="/about" element={<About />} />
+
+            
           </Routes>
         </BrowserRouter>
         <ToastContainer
---
+++ src/Pages/About.js
@@ -0,0 +1,73 @@
+import React, { useEffect } from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import ScrollReveal from 'scrollreveal';
+import './About.css';
+import { BiLogoLinkedin, BiLogoGithub } from "react-icons/bi";
+import { Link } from 'react-router-dom';
+import About1 from '../images/About1.png';
+import About2 from '../images/About2.png';
+
+function About() {
+  useEffect(() => {
+    const sr = ScrollReveal({
+      origin: 'top',
+      distance: '80px',
+      duration: 2000,
+      reset: true
+    });
+
+    sr.reveal('.new-about-main-card', { interval: 200 });
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className='new-about-main-section'>
+        <div className='new-about-main-div'>
+          <div className='new-about-main-card'>
+            <div className='new-about-card'>
+              <div className='new-about-card-image'>
+                <img src={About1} alt='Ayush Tripathi'></img>
+              </div>
+              <div className='new-about-card-info'>
+                <h1>Ayush Tripathi</h1>
+                <h6>Full Stack Developer</h6>
+                <div className='new-about-card-icons'>
+                  <Link to="https://www.linkedin.com/in/ayush-tripathi-039a20220" target='_blank'>
+                    <BiLogoLinkedin className='new-about-logo' />
+                  </Link>
+                  <Link to="https://www.github.com/ayushtripathi12103634" target='_blank'>
+                    <BiLogoGithub className='new-about-logo' />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='new-about-main-card'>
+            <div className='new-about-card'>
+              <div className='new-about-card-image'>
+                <img src={About2} alt='Mohak Tiwari'></img>
+              </div>
+              <div className='new-about-card-info'>
+                <h1>Mohak Tiwari</h1>
+                <h6>Full Stack Developer</h6>
+                <div className='new-about-card-icons'>
+                  <Link to="https://www.linkedin.com/in/mohak-tiwari-b78198226/" target='_blank'>
+                    <BiLogoLinkedin className='new-about-logo' />
+                  </Link>
+                  <Link to="https://www.github.com/mohak1301" target='_blank'>
+                    <BiLogoGithub className='new-about-logo' />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default About;
