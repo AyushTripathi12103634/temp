4615a3cf36069634310dd47a7296bb5201e9fa5c
Mohak1301 <mohaktiwari2004@gmail.com>
2024-08-10 17:41:48 +0530

    navbar bugs fixed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -25,79 +25,103 @@ function NavBar() {
   const handleClick = () => setClick(!click);
 
   return (
-    <>
-      <nav className="navbar">
-        <div className="nav-container">
-          <NavLink to="/" className="nav-logo">
+    <nav className="navbar">
+      <div className="nav-container">
+        <div className="nav-logo">
+          <NavLink to="/">
             <img src={SSLogo} alt="SquadScript Logo" />
           </NavLink>
-
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <NavLink to="/" className="nav-links" onClick={() => setClick(false)}>
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/about" className="nav-links" onClick={() => setClick(false)}>
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/join" className="nav-links" onClick={() => setClick(false)}>
-                Join
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/contact" className="nav-links" onClick={() => setClick(false)}>
-                Contact Us
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              {username ? (
-                <NavLink to="/profile" className="nav-links" onClick={() => setClick(false)}>
-                  {username}
-                </NavLink>
-              ) : (
-                <NavLink to="/login-signup" className="nav-links" onClick={() => setClick(false)}>
-                  Login
-                </NavLink>
-              )}
-            </li>
-          </ul>
-
-          <div className="dark_mode">
-            <input
-              className="dark_mode_input"
-              type="checkbox"
-              id="flexSwitchCheckDefault"
-              onChange={handlethemechange}
-            />
-            <label
-              className={`dark_mode_label form-check-label text-${
-                mode === "light" ? "dark" : "light"
-              }`}
-              htmlFor="flexSwitchCheckDefault"
-            >
-              <Sun />
-              <Moon />
-            </label>
-          </div>
-
-          <div className="nav-icon me-5" onClick={handleClick}>
-            {click ? (
-              <span className="icon">
-                <HamburgetMenuClose />
-              </span>
-            ) : (
-              <span className="icon">
-                <HamburgetMenuOpen />
-              </span>
-            )}
-          </div>
         </div>
-      </nav>
-    </>
+
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <NavLink
+              to="/"
+              className="nav-links"
+              onClick={() => setClick(false)}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/about"
+              className="nav-links"
+              onClick={() => setClick(false)}
+            >
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/join"
+              className="nav-links"
+              onClick={() => setClick(false)}
+            >
+              Join
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/contact"
+              className="nav-links"
+              onClick={() => setClick(false)}
+            >
+              Contact Us
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            {username ? (
+              <NavLink
+                to="/profile"
+                className="nav-links"
+                onClick={() => setClick(false)}
+              >
+                {username}
+              </NavLink>
+            ) : (
+              <NavLink
+                to="/login-signup"
+                className="nav-links"
+                onClick={() => setClick(false)}
+              >
+                Login
+              </NavLink>
+            )}
+          </li>
+        </ul>
+
+        <div className="dark_mode">
+          <input
+            className="dark_mode_input"
+            type="checkbox"
+            id="flexSwitchCheckDefault"
+            onChange={handlethemechange}
+          />
+          <label
+            className={`dark_mode_label form-check-label text-${
+              mode === "light" ? "dark" : "light"
+            }`}
+            htmlFor="flexSwitchCheckDefault"
+          >
+            <Sun />
+            <Moon />
+          </label>
+        </div>
+
+        <div className="nav-icon me-5" onClick={handleClick}>
+          {click ? (
+            <span className="icon">
+              <HamburgetMenuClose />
+            </span>
+          ) : (
+            <span className="icon">
+              <HamburgetMenuOpen />
+            </span>
+          )}
+        </div>
+      </div>
+    </nav>
   );
 }
 
